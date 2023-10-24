7a26eda11565acb2f9f26af5cfe076e2f4a6f078
yinyangwarrior0928 <codecoolcodeawesome@gmail.com>
2023-10-24 10:25:06 +0300

    add: project

---
+++ break-does-not-work-resolution/break-does-not-work-resolution.cpp
@@ -0,0 +1,39 @@
+// @author: yinyang_warrior
+// @objective: stackoverflow question resolved
+// @reference: https://stackoverflow.com/questions/77345395/break-does-not-wortk
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+    char words[1000][100];
+    int i = 0;
+
+    // Read words until Enter key is pressed
+    while (1) {
+        if (scanf("%99s", &words[i][0]) == 1) {
+            i++;
+        }
+        else {
+            break; // Exit the loop when no more words are entered
+        }
+    }
+
+   for (int s = 0; s < i; s++) {
+        int length = strlen(words[s]);
+
+        // Reverse the word
+        for (int j = 0; j < length / 2; j++) {
+            char temp = words[s][j];
+            words[s][j] = words[s][length - 1 - j];
+            words[s][length - 1 - j] = temp;
+        }
+
+        // Print the reversed word
+        printf("%s\n", words[s]);
+    }
+
+    return 0;
+}
